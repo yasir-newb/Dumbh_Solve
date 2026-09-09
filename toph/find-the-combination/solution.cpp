df6e6e1154210fc822343e1e287721445ff0e89a
M Abdullah Yasir Tomal <mhmmdyasir1990@gmail.com>
2026-09-09 23:45:12 +0600

    Solve: Find the Combination! [Accepted] (C++)

---
+++ toph/find-the-combination/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+void solve() {
+    cout<<"!";
+}
+
+int main() {
+    fast_io;
+    
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
